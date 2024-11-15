cf37758df73a45e2794d4ba0c37121471b1cb19d
Julia Cassemiro <juliacassemiro@outlook.com>
2024-11-15 19:43:20 -0300

    refactor: architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
-import { PlayersModule } from './players/players.module';
-import { TeamsModule } from './teams/teams.module';
+import { PlayersModule } from './modules/players/players.module';
+import { TeamsModule } from './modules/teams/teams.module';
 
 @Module({
   imports: [DatabaseModule, PlayersModule, TeamsModule],
